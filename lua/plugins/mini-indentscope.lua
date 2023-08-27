3ee538485f6caa890b92966a0488f95f6dbd4c27
alinademi <nademi@gmail.com>
2023-08-27 16:54:58 -0700

    add/ config for indent linesactually changing the default lazyvim settings

---
+++ lua/plugins/mini-indentscope.lua
@@ -0,0 +1,11 @@
+return {
+   'echasnovski/mini.indentscope',
+   opts = {
+      -- Draw options
+      draw = {
+         -- Delay (in ms) between event and start of drawing scope indicator
+         delay = 20,
+      },
+      symbol = '╎',
+   }
+}
