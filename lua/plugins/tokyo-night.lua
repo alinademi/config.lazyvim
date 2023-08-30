2185cd57a06bc9e8845ff6e70b5d46fc8a8dce32
alinademi <nademi@gmail.com>
2023-08-30 14:27:23 -0700

    mod/ bg color for tokyo night due to change of monitor color profile

--- lua/plugins/tokyo-night.lua
+++ lua/plugins/tokyo-night.lua
@@ -1,29 +1,28 @@
 return {
-   {
-      "folke/tokyonight.nvim",
-      opts = {
-         -- transparent = true,
-         styles = {
-            functions = { italic = true },
-            -- sidebars = "transparent",
-            -- floats = "transparent",
-         },
-         -- for a list of highlights see https://github.com/folke/tokyonight.nvim/blob/main/extras/lua/tokyonight_night.lua
-         on_highlights = function(highlights, colors)
-            highlights.String  = { fg = "#6faee2", bg = colors.bg }
-            highlights.Comment = { fg = "#5b6aa0", bg = colors.bg }
-         end,
-         on_colors = function(colors)
-            colors.bg = "#01040A"
-         end,
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      -- transparent = true,
+      styles = {
+        functions = { italic = true },
+        -- sidebars = "transparent",
+        -- floats = "transparent",
       },
-   },
-
-   -- Configure LazyVim to load theme
-   {
-      "LazyVim/LazyVim",
-      opts = {
-         colorscheme = "tokyonight-night",
-      },
-   }
+      -- for a list of highlights see https://github.com/folke/tokyonight.nvim/blob/main/extras/lua/tokyonight_night.lua
+      on_highlights = function(highlights, colors)
+        highlights.String = { fg = "#6faee2", bg = colors.bg }
+        highlights.Comment = { fg = "#5b6aa0", bg = colors.bg }
+      end,
+      on_colors = function(colors)
+        -- colors.bg = "#0F111A"
+      end,
+    },
+  },
+  -- Configure LazyVim to load theme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+    },
+  },
 }
