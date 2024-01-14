1604d8ef3f0525efc4558dc5d07beee9fd2d7129
alinademi <nademi@gmail.com>
2024-01-14 12:39:49 -0800

    fix/ temporarily disabled config for indent-blank line

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,20 +1,20 @@
 vim.cmd [[highlight IndentBlanklineIndent1 guibg=#0f111a gui=nocombine]]
 vim.cmd [[highlight IndentBlanklineIndent2 guibg=#10121B gui=nocombine]]
 return {
-   {
-      "lukas-reineke/indent-blankline.nvim",
-      event = { "BufReadPost", "BufNewFile" },
-      opts = {
-         char = "",
-         char_highlight_list = {
-            "IndentBlanklineIndent1",
-            "IndentBlanklineIndent2",
-         },
-         space_char_highlight_list = {
-            "IndentBlanklineIndent1",
-            "IndentBlanklineIndent2",
-         },
-         show_trailing_blankline_indent = true,
-      },
-   }
+  {
+    -- "lukas-reineke/indent-blankline.nvim",
+    -- event = { "BufReadPost", "BufNewFile" },
+    -- opts = {
+    --   char = "",
+    --   char_highlight_list = {
+    --     "IndentBlanklineIndent1",
+    --     "IndentBlanklineIndent2",
+    --   },
+    --   space_char_highlight_list = {
+    --     "IndentBlanklineIndent1",
+    --     "IndentBlanklineIndent2",
+    --   },
+    --   show_trailing_blankline_indent = true,
+    -- },
+  }
 }
