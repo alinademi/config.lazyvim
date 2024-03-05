6ff44ab59dfe99d36d415aff2a5f581714284a01
alinademi <nademi@gmail.com>
2024-03-05 14:14:01 -0800

    add/ fast-cursor plugin
    prevevent unfolding the folded areas
    when cursor passes
    
    add/ editor config
    add/ explicitly enabled lazy for vscode

---
+++ lua/plugins/vscode-multi-cursor.lua
@@ -0,0 +1,31 @@
+return {
+  'vscode-neovim/vscode-multi-cursor.nvim',
+  event = 'VeryLazy',
+  cond = not not vim.g.vscode,
+  opts = {},
+  config = function()
+    require('vscode-multi-cursor').setup { -- Config is optional
+      -- Whether to set default mappings
+      -- default_mappings = true,
+      -- If set to true, only multiple cursors will be created without multiple selections
+      no_selection = false,
+    }
+
+    -- keymaps
+    local cursors = require 'vscode-multi-cursor'
+
+    vim.keymap.set({ 'n', 'x', 'i' }, '<C-d>', function()
+      require('vscode-multi-cursor').addSelectionToNextFindMatch()
+    end, { desc = 'use <C-d> to add selection to next find match' })
+    vim.keymap.set({ 'n', 'x' }, 'mc', cursors.create_cursor, { expr = true, desc = 'Create cursor' })
+    vim.keymap.set({ 'n' }, 'mcc', cursors.cancel, { desc = 'Cancel/Clear all cursors' })
+    vim.keymap.set({ 'n', 'x' }, 'mi', cursors.start_left, { desc = 'Start cursors on the left' })
+    vim.keymap.set({ 'n', 'x' }, 'mI', cursors.start_left_edge, { desc = 'Start cursors on the left edge' })
+    vim.keymap.set({ 'n', 'x' }, 'ma', cursors.start_right, { desc = 'Start cursors on the right' })
+    vim.keymap.set({ 'n', 'x' }, 'mA', cursors.start_right, { desc = 'Start cursors on the right' })
+    vim.keymap.set({ 'n' }, '[mc', cursors.prev_cursor, { desc = 'Goto prev cursor' })
+    vim.keymap.set({ 'n' }, ']mc', cursors.next_cursor, { desc = 'Goto next cursor' })
+    vim.keymap.set({ 'n' }, 'mcs', cursors.flash_char, { desc = 'Create cursor using flash' })
+    vim.keymap.set({ 'n' }, 'mcw', cursors.flash_word, { desc = 'Create selection using flash' })
+  end,
+}
--- lua/vscode/vscode-plugin-settings.lua
+++ lua/vscode/vscode-plugin-settings.lua
@@ -4,11 +4,15 @@
 -- https://www.lazyvim.org/extras/vscode
 return {
   --disable even those that lazyvim loads by default in vscode
-  { "ggandor/flit.nvim", vscode = false },
-  { "ggandor/leap.nvim", vscode = false },
-  { "echasnovski/mini.ai", vscode = false },
-  { "echasnovski/mini.comment", vscode = false },
-  { "echasnovski/mini.pairs", vscode = false },
+  { "ggandor/flit.nvim",                      vscode = false },
+  { "ggandor/leap.nvim",                      vscode = false },
+  { "echasnovski/mini.ai",                    vscode = false },
+  { "echasnovski/mini.comment",               vscode = false },
+  { "echasnovski/mini.pairs",                 vscode = false },
   -- enable in vscode
-  { "echasnovski/mini.surround", vscode = true },
+  { "lazy.nvim",                              vscode = true },
+  { "echasnovski/mini.surround",              vscode = true },
+  { "vscode-neovim/vscode-multi-cursor.nvim", vscode = true },
+  { "xiyaowong/fast-cursor-move.nvim",        vscode = true },
+
 }
